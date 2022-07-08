6e649a36c37b16ca70d33f7cac6eec6ff1f5f387
Joaquin Granado <joaquingranado98@gmail.com>
2022-07-07 21:08:17 -0300

    agregado menu administrador 1,2 terminado menu cliente 1 terminado

--- proyecto/src/supermerk/Producto.java
+++ proyecto/src/supermerk/Producto.java
@@ -1,4 +1,10 @@
 package supermerk;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Producto {
@@ -31,7 +37,10 @@ public class Producto {
 			this.marca="No tiene";
 			this.descripcion="No tiene";
 		}
-		
+		// CONSTRUCTOR 3
+		public Producto() {
+			
+		}
 	// metodos PRODUCTO
 	//       MOSTRAR UN PRODUCTO	
 	public void mostrarProducto() {
@@ -42,7 +51,32 @@ public class Producto {
 		System.out.println(getCantidad());
 		System.out.println(getDescripcion());
 	}
-	
+//////////////////  GUSTAVO ////////////////////////////////////////////
+	public void capturaDatosProducto() {
+		try {
+			
+		
+		@SuppressWarnings("resource")
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Ingresar nombre  del producto: ");
+		this.nombre=teclado.nextLine();
+		System.out.println("Ingresar marca  del producto: ");
+		this.marca=teclado.nextLine();
+		System.out.println("Ingresar precio unitario  del producto: ");
+		this.precioUnitario=teclado.nextFloat();
+		teclado.nextLine(); //LIMPIAMOS EL BUFFER 
+		System.out.println("Ingresar descripcion  del producto: ");
+		this.descripcion=teclado.nextLine();
+		System.out.println("Ingresar cantidad  del producto: ");
+		this.cantidad=teclado.nextInt();
+		teclado.nextLine(); //LIMPIAMOS EL BUFFER
+		}
+		catch(Exception e) {
+			System.out.println("Error capturando datos del nuevo producto"+ e.getMessage()); 
+		}
+		
+	}
+/////////////////////////////GUSTAVO/////////////////////////////////////
 	// EDITAR UN PRODUCTO
 	public void modificarPrecio() {
 		float precioNuevo;
@@ -51,9 +85,32 @@ public class Producto {
 				getNombre());
 		precioNuevo=teclado.nextFloat();
 		setPrecioUnitario(precioNuevo);
-		System.out.println("Se a modificado el precio correctamente");
+		System.out.println("Se ha modificado el precio correctamente");
 	}
+//////////////////GUSTAVO ////////////////////////////////////////////
+	public void listarProductos(ResultSet rs) {
 	
+		
+		//PASO 5: Extraer datos ResultSet
+		try {
+			if(rs.isBeforeFirst()) {
+				while(rs.next()) {
+				
+					// Recibir por tipo de columna
+					String nombre = rs.getString("nombre");
+					String precio = rs.getString("precioUnitario");
+					String marca = rs.getString("marca");
+					System.out.println("Nombre: " + nombre + ", Precio: " + precio + ", Marca: " + marca );
+				}
+				}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("hay errroirororororororororo");
+			e.printStackTrace();
+		}
+	
+	}
+/////////////////////////////GUSTAVO/////////////////////////////////////
 	//     GETTER SETTER
 	public int getId() {
 		return id;
--- proyecto/src/test/TestMain.java
+++ proyecto/src/test/TestMain.java
@@ -8,28 +8,30 @@ import java.util.Scanner;
 import supermerk.*;
 
 public class TestMain {
+
+	//Registrar la JDBC Driver
+	//JDBC nombre del driver y URL de la BDD
+	static final String JDBC_DRIVER ="com.mysql.cj.jdbc.Driver";
+	//static final String DB_URL="jdbc:mysql://localhost:3306/supermarkBaseDatos";
+	static final String DB_URL="jdbc:mysql://localhost:3306/supermercadoUno";
+
+	// Credenciales de la BDD
+	static final String USER = "root";
+	static final String PASS= "jag40515989";
+
+
+	//variables globales
+	static int id_cliente;
+	static int id_usuario;
 	
-		//Registrar la JDBC Driver
-		//JDBC nombre del driver y URL de la BDD
-		static final String JDBC_DRIVER ="com.mysql.cj.jdbc.Driver";
-		//static final String DB_URL="jdbc:mysql://localhost:3306/supermarkBaseDatos";
-		static final String DB_URL="jdbc:mysql://localhost:3306/supermercadoUno";
-		
-		// Credenciales de la BDD
-		static final String USER = "root";
-		static final String PASS= "jag40515989";
-		
-		
-		//variables globales
-		static int id_cliente;
-		static int id_usuario;
+	
 
 	public static void main(String[] args) {
-		
-		
+
+
 		/*  VARIABLES PARA JAVA  */
 		Cliente usuario = null;
-		
+
 		/// ---- datos cliente para registrarse
 		String nombreUsuario;
 		String password;
@@ -43,38 +45,40 @@ public class TestMain {
 		String pais;
 		String provincia;
 		int codigoPostal;
-		
+
 		Scanner teclado = new Scanner(System.in);
-		
+
 		int opcion, opcionCliente;
 		int seleccion;
-		
-		
+
+		// variable local main gustavo (administrador menu)
+		int opcionProducto;
+
 		/*  CONEXIONES Y REGISTROS MYSQL */
 		// creamos variable String sql para escribir en codigo sql
 		//dentro de java el objeto 'ResultSet = SELECT'
 		String sql;
 		ResultSet rs = null;
-		
+
 		//registrar el driver
 		Connection conn = null;
 		Statement stmt = null;
 		PreparedStatement ps = null;
-		
+
 		try {
 			//PASO2: Regristramos JDBC Driver
 			Class.forName(JDBC_DRIVER);
-			
+
 			//PASO 3: Abrir una Conexion
 			System.out.println("Conectando a la Base de Datos...");
 			conn = DriverManager.getConnection(DB_URL,USER,PASS);
-			
+
 			//PASO 4: Ejecutar un consultor SQL con el objeto Statement
 			System.out.println("Creando Statement...");
 			stmt = conn.createStatement();
-			
-						
-					/* MENU DE LA APLICACION*/
+
+
+			/* MENU DE LA APLICACION*/
 			do {
 				opcion=inicioSesion();
 				switch (opcion) {
@@ -91,26 +95,26 @@ public class TestMain {
 					apellido=teclado.nextLine();
 					System.out.println("DNI: ");
 					dni=teclado.nextInt();
-					
+
 					teclado.nextLine(); //LIMPIAMOS EL BUFFER (para evitar que el enter quede como caracter y poder igresar el siguiente String)
-									
+
 					//dentro de java el objeto 'ResultSet = insert'
 					//CARGANDO EL  USUARIO
 					sql = "insert into Usuario (nombreUsuario, password, permisos)values(?,?,?)";
-					
+
 					/*"('" + nombreUsuario + "','" + password + "',"+ 0 + ")";
 					rs = stmt.executeQuery(sql);*/
-					
+
 					ps = conn.prepareStatement(sql); 
 					ps.setString(1, nombreUsuario);
 					ps.setString(2, password);
 					ps.setInt(3, 0);
 					ps.execute();
-					
+
 					// BUSCAR ID USUARIO
 					sql = "SELECT * from Usuario";
 					rs = stmt.executeQuery(sql);
-					
+
 					//PASO 5: Extraer datos ResultSet
 					if(rs.isBeforeFirst()) {
 						while(rs.next()) {
@@ -123,8 +127,8 @@ public class TestMain {
 					}
 					else
 						id_usuario = 1;
-					
-					
+
+
 					//cargamos cliente
 					sql = "insert into Cliente (nombre, apellido, dni, cliente_es_usuario)values(?,?,?,?)";
 					ps = conn.prepareStatement(sql); 
@@ -133,27 +137,27 @@ public class TestMain {
 					ps.setInt(3, dni);
 					ps.setInt(4, id_usuario);
 					ps.execute();
-							
+
 					/*"('" + nombre + "','" + apellido  + "'," + dni + ")";
 					rs = stmt.executeQuery(sql); */
-					
-					
+
+
 					/* FALTA AGREGAR LOS DATOS DE LA TABLA Usuario*/
-					
+
 					break;
 				}
 				case 2:{ 	// INICIAR SESION
-					
+
 					System.out.println("Ingrese los siguientes datos");
 					System.out.println("Nombre de Usuario: ");
 					nombreUsuario=teclado.nextLine();
 					System.out.println("Password: ");
 					password=teclado.nextLine();
-					
+
 					// Consulta SQL
 					sql = "SELECT * from Usuario";
 					rs = stmt.executeQuery(sql);
-					
+
 					//PASO 5: Extraer datos ResultSet
 					if(rs.isBeforeFirst()) {
 						while(rs.next()) {
@@ -161,10 +165,10 @@ public class TestMain {
 							String nombreUser = rs.getString("nombreUsuario");
 							String contrasena = rs.getString("password");
 							int permisos = rs.getInt("permisos");
-							
+
 							if(nombreUser.equals(nombreUsuario) && contrasena.equals(password)) {													
 								if(permisos==0){ // MENU DEL CLIENTE
-									
+
 									/*		CONSEGUIMOS EL ID CLIENTE Y USUARIO QUE HAYA INICIADO SESION 	*/
 									id_usuario = rs.getInt("idUsuairo");			//
 									sql = "SELECT * from Cliente";					//
@@ -177,7 +181,7 @@ public class TestMain {
 												id_cliente = rs.getInt("idCliente");//
 										}
 									}
-									
+
 									/* 		CONTINUAMOS CON EL MENU CLIENTE 		*/
 									do {
 										System.out.println("ERES CLIENTE");
@@ -187,15 +191,15 @@ public class TestMain {
 											// Consulta SQL
 											sql = "SELECT * from Producto";
 											rs = stmt.executeQuery(sql);
-													
+
 
 											int contadorDeProductos = 0;
-											
+
 											//PASO 5: Extraer datos ResultSet
 											if(rs.isBeforeFirst()) {
 												while(rs.next()) {
-													
-													
+
+
 													// Recibir por tipo de columna
 													int idProducto = rs.getInt("idProducto");
 													String nombreProducto = rs.getString("nombre");
@@ -203,32 +207,28 @@ public class TestMain {
 													float preciUnitario = rs.getFloat("precioUnitario");
 													String marca = rs.getString("marca");
 													String descripcion = rs.getString("descripcion");
-													
+
 													System.out.println("---------------------------------");
 													System.out.println(idProducto + ")" + "Producto: " + nombreProducto + "( " + marca + ")" + "\n" + 
-																	   "Precio: " + preciUnitario + "\n" + 
-																	   "Cantidad: " + cantidad + "\n" +
-																	   "Descripcion: " + descripcion + "\n" ); 
+															"Precio: " + preciUnitario + "\n" + 
+															"Cantidad: " + cantidad + "\n" +
+															"Descripcion: " + descripcion + "\n" ); 
 													System.out.println("---------------------------------");
 												}
-												
+
 												while(opcionCliente == 1) {
 													System.out.println("Ingrese el numero del producto que quiere comprar");
 													seleccion = teclado.nextInt();
-													
-													if(rs.isBeforeFirst()) {
-														System.out.println("leera el primer elemento");
-													}
-													else {
-														System.out.println("se paso del primer elemento");
-													}
-													
-													sql = "select count(p.idProducto) from producto p";
+													teclado.nextLine(); //LIMPIAMOS EL BUFFER
+
+													//sql = "select count(p.idProducto) from producto p";
+													sql = "select * from Producto";
 													rs = stmt.executeQuery(sql);
-														
+
 													while(rs.next()) {
-														contadorDeProductos = rs.getInt("count(p.idProducto)");
+														contadorDeProductos = rs.getInt("idProducto");	
 													}
+												
 													if(seleccion <= contadorDeProductos && seleccion >= 1) {
 														sql = "insert into Ticket (Cliente_idCliente, Cliente_idUsuario, Producto_idProducto)values(?,?,?)";
 														ps = conn.prepareStatement(sql); 
@@ -236,16 +236,17 @@ public class TestMain {
 														ps.setInt(2, id_usuario);
 														ps.setInt(3, seleccion);
 														ps.execute();
-														
+
 														System.out.println("Agregado al carrito");
 													}
 													else {
-															System.out.println("No existe el producto elegido");
-														}
+														System.out.println("No existe el producto elegido");
+													}
 													System.out.println("1)Continuar seleccionando. \n2)Salir.");
 													opcionCliente = teclado.nextInt();
+													teclado.nextLine(); //LIMPIAMOS EL BUFFER
 												}
-												
+
 											}
 											else {
 												System.out.println("No hay Productos en Venta");
@@ -254,17 +255,120 @@ public class TestMain {
 										case 2:	//2)Ver lista de productos
 											break;
 										case 3:	//3)Autorizar compra
+
 											break;
 										}
 									}while(opcionCliente != 0);
 								}
-								
-								
+
+
 								else { // MENU DEL ADMINISTRADOR
 									System.out.println("ERES ADMIN");
 									opcionCliente = menuAdministrador();
 									switch (opcionCliente) {
 									case 1: //1)Cargar productos
+										///////////////////////////////////////////// GUSTAVO ///////////////////////////////////////////////////////////////
+
+										do {
+											opcionProducto = menuProducto();
+											Producto prod = new Producto();
+											switch(opcionProducto) {
+
+											case 1:
+
+												sql = "select * from producto";
+
+												rs = stmt.executeQuery(sql);
+												prod.listarProductos(rs);
+
+												break;
+											case 2:
+
+												prod.capturaDatosProducto();
+
+												sql = "INSERT INTO producto (nombre,cantidad,precioUnitario,marca,descripcion) VALUES (?,?,?,?,?)";
+												ps = conn.prepareStatement(sql); 
+												ps.setString(1, prod.getNombre());
+												ps.setInt(2, prod.getCantidad());
+												ps.setFloat(3,prod.getPrecioUnitario());
+												ps.setString(4, prod.getMarca());
+												ps.setString(5, prod.getDescripcion());
+												ps.execute();
+												break;
+											case 3:
+												System.out.println("Ingresar codigo  del producto: ");
+												int idP=teclado.nextInt();
+												sql = "select * from producto WHERE idProducto="+idP;
+
+												rs = stmt.executeQuery(sql);
+												while(rs.next()){
+													prod.setId(rs.getInt("idProducto"));
+													prod.setNombre(rs.getString("nombre"));
+													prod.setCantidad(rs.getInt("cantidad"));
+													prod.setPrecioUnitario(rs.getFloat("precioUnitario"));
+													prod.setMarca(rs.getString("marca"));
+													prod.setDescripcion(rs.getString("descripcion"));
+												}
+												rs.close();
+												String opcionMod = "NO";
+
+
+												prod.mostrarProducto();
+												System.out.println("Desea modificar el Producto SI/NO: ");
+												opcionMod=teclado.next();
+
+												if(opcionMod.equals("SI")) {
+													prod.capturaDatosProducto();
+													sql = "UPDATE producto SET nombre = ?, cantidad = ?,precioUnitario =?, marca =?,descripcion=? WHERE idProducto=?";
+													ps = conn.prepareStatement(sql); 
+													ps.setString(1, prod.getNombre());
+													ps.setInt(2, prod.getCantidad());
+													ps.setFloat(3,prod.getPrecioUnitario());
+													ps.setString(4, prod.getMarca());
+													ps.setString(5, prod.getDescripcion());
+													ps.setInt(6, prod.getId());
+													ps.executeUpdate();
+												}
+
+												break;
+											case 4:
+												System.out.println("Ingresar codigo  del producto: ");
+												int idPD=teclado.nextInt();
+												teclado.nextLine(); //LIMPIAMOS EL BUFFER
+												sql = "select * from producto WHERE idProducto="+idPD;
+
+												rs = stmt.executeQuery(sql);
+												while(rs.next()){
+													prod.setId(rs.getInt("idProducto"));
+													prod.setNombre(rs.getString("nombre"));
+													prod.setCantidad(rs.getInt("cantidad"));
+													prod.setPrecioUnitario(rs.getFloat("precioUnitario"));
+													prod.setMarca(rs.getString("marca"));
+													prod.setDescripcion(rs.getString("descripcion"));
+												}
+												//rs.close();
+												String opcionDel = "NO";
+												prod.mostrarProducto();
+												System.out.println("Desea eliminar el Producto SI/NO: ");
+												opcionDel=teclado.next();
+
+												if(opcionDel.equals("SI")) {
+													try {
+														sql = "DELETE FROM Producto WHERE idProducto ="+idPD;
+														stmt.executeUpdate(sql);
+														sql = "select * from producto";
+														rs = stmt.executeQuery(sql);
+														prod.listarProductos(rs);
+													}catch (Exception e) {
+														System.out.println("No se pudo eliminar el producto esta uso en un carrito");
+													}
+												}
+												break;
+											}
+
+										}while(opcionProducto != 0);
+
+										////////////////////////////////////// GUSTAVO ///////////////////////////////////////
 										break;
 									case 2:	//2)Ver todos los usuarios que realizaron una compra
 										sql = "select u.idUsuairo, u.nombreUsuario from usuario u inner join ProductoComprado p on u.idUsuairo = p.Usuario_idUsuario";
@@ -283,31 +387,46 @@ public class TestMain {
 										}
 										break;
 									case 3: //3)Ver listado de productos seleccionados por el usuario
+										sql = "select u.idUsuairo, u.nombreUsuario from usuario u inner join Cliente c on u.idUsuairo = c.Usuario_idUsuario";
+										rs = stmt.executeQuery(sql);
+										int contadorDeUsuario = 0;
+										//PASO 5: Extraer datos ResultSet
+										if(rs.isBeforeFirst()) {
+											while(rs.next()) {
+												// Recibir por tipo de columna
+												int idUsuario = rs.getInt("u.idUsuairo");
+												String nombreCliente = rs.getString("u.nombreUsuario");
+												mostrar_un_Usuario(idUsuario, nombreCliente);
+												contadorDeUsuario++;
+											}
+											id_usuario = elegirUsuario(contadorDeUsuario);
+											sql = "select u.idUsuairo, u.nombreUsuario from usuario u inner join Cliente c on u.idUsuairo = c.Usuario_idUsuario";
+											rs = stmt.executeQuery(sql);
+										}
+										else {
+											System.out.println("Aun no se ha realizado ninguna compra.");
+										}
 										break;
 									}
-									
-		
-									
-									
 								}
 							}
 							else {
 								System.out.println("Su Nombre de Usuario o contraseña es incorrecto");
 							}
-							
+
 						}
 					}
 					else {
 						System.out.println("No Hay Usuarios Creados");
 					}
-				
+
 					break;
 				}
-				
+
 				}// final switch Registrarse | Iniciar sesion 
-				
+
 			} while (opcion!=0);
-			
+
 			//PASO 6: Entorno de limpieza
 			rs.close();
 			stmt.close();
@@ -339,7 +458,7 @@ public class TestMain {
 				se.printStackTrace();
 			}// cierra finally try
 		} // cierra try
-		
+
 	}
 
 	public static int inicioSesion() {
@@ -354,7 +473,7 @@ public class TestMain {
 		}while(x<0 || x>2);
 		return x;
 	}
-	
+
 	public static int menuCliente() {
 		int x;
 		Scanner teclado = new Scanner(System.in);
@@ -368,7 +487,7 @@ public class TestMain {
 		}while(x<0 || x>3);
 		return x;
 	}
-	
+
 	public static int menuAdministrador() {
 		int x;
 		Scanner teclado = new Scanner(System.in);
@@ -382,11 +501,38 @@ public class TestMain {
 		}while(x<0 || x>3);
 		return x;
 	}
-	
-	
+
+
 	/// 	MOSTRAR CADA USUARIO
 	public static void mostrar_un_Usuario(int id, String nombre) {
 		System.out.println("Nro: " + id + " Nombre: " + nombre);
+	}
+	// SELECCIONAR UN USUARIO
+	public static int elegirUsuario(int n) {
+		int opcion;
+		Scanner teclado = new Scanner(System.in);
+		do {
+			System.out.println("Ingresar el numero del cliente");
+			opcion = teclado.nextInt();
+		}while(opcion > n || opcion < 1);
+		return opcion;
+	}
+
+
+	// GUSTAVO
+	public static int menuProducto() {
+		int x;
+		Scanner teclado = new Scanner(System.in);
+		do {
+			System.out.println("Ingrese una opcion:");
+			System.out.println("1)Listar Productos");
+			System.out.println("2)Agregar Producto");
+			System.out.println("3)Modificar Producto");
+			System.out.println("4)Eliminar Producto");
+			System.out.println("0)Menu Principal");
+			x=teclado.nextInt();
+		}while(x<0 || x>4);
+		return x;
 	}
 }
 
